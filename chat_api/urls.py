6676e2ec654e368e8d8cffe0bf4d43ac3dad44f1
Keeya Emmanuel <keeyaemmanuel12@gmail.com>
2020-09-20 16:37:19 +0300

    add gitignore

--- chat_api/urls.py
+++ chat_api/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# Url patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chat', include('chat.urls')),
